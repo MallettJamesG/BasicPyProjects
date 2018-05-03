25c96b98bf8c0deb85f10cd17d805ece9abb1944
MallettJamesG <MallettJamesG@gmail.com>
2018-05-03 08:40:05 +0200

    Working on Magic8Ball GUI

--- 2.Magic8Ball/Magic8Ball.py
+++ 2.Magic8Ball/Magic8Ball.py
@@ -1,6 +1,6 @@
 import time
 import random
-
+from tkinter import *
 
 responses=[]
 responses.append("No")
@@ -20,17 +20,60 @@ responses.append("I think it could happen")
 responses.append("Try something else")
 responses.append("Try asking something else")
 
-while True:
+class TheGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Python Magic 8 Ball GUI")
 
-    the_q = input("\nEnter your question (no to exit): ")
-    if the_q == "no":
-        time.sleep(1)
-        print("\nOkay, Goodbye..")
-        time.sleep(1)
-        break
-    print("\n --- Let me think for a moment ---\n")
-    for i in range(5):
-        print((i+1)*".")
-        time.sleep(1)
+        self.label = Label(self.master, text="Enter a Question you would like to ask (no to quit):")
+        self.label.pack()
 
-    print("\n",random.choice(responses))
+        self.enterplace = Entry(self.master)
+        self.enterplace.pack()
+
+        self.labeltext=StringVar()
+        self.labeltext.set("--")
+        self.response = Label(self.master,justify = CENTER,textvariable=self.labeltext)
+        self.response.pack()
+
+        self.frame = Frame(self.master)
+        self.frame.pack()
+
+        self.greet_button = Button(self.frame, text="Ask", command=self.ask)
+        self.greet_button.pack(side = LEFT)
+
+        self.close_button = Button(self.frame, text="Clear", command=self.clear)
+        self.close_button.pack(side = LEFT)
+
+        self.close_button = Button(self.frame, text="Play Again", command=self.clear)
+        self.close_button.pack(side = LEFT)
+
+        self.close_button = Button(self.frame, text="Close", command=master.quit)
+        self.close_button.pack(side = LEFT)
+
+    def ask(self):
+        the_q = self.enterplace.get()
+
+        if the_q == "no":
+            self.labeltext.set("Okay, Goodbye..")
+            time.sleep(2)
+            self.master.quit
+        else:
+            self.labeltext.set("Let me think for a moment")
+            self.master.update_idletasks()
+            time.sleep(2)
+            self.labeltext.set("---")
+            self.master.update_idletasks()
+            time.sleep(2)
+            self.labeltext.set(random.choice(responses))
+            self.master.update_idletasks()
+            time.sleep(2)
+
+    def clear(self):
+        self.labeltext.set("")
+        self.master.update_idletasks()
+
+
+root = Tk()
+my_gui = TheGUI(root)
+root.mainloop()
